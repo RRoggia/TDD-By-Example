8031ffa6650ef0e30f5bda6b8a812ec346ac05ab
Roggia <renan.roggia@sap.com>
2016-02-08 19:44:07 -0200

    create test dolar multiplication

--- src/com/rroggia/tddbyexample/financial/MoneyTest.java
+++ src/com/rroggia/tddbyexample/financial/MoneyTest.java
@@ -2,4 +2,10 @@ package com.rroggia.tddbyexample.financial;
 
 public class MoneyTest {
 
+	
+	public void testMultiplication(){
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
 }
