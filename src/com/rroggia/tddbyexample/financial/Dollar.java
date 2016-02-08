474300580f51213080cb365f782ec1661fab42ff
Roggia <renan.roggia@sap.com>
2016-02-08 19:55:24 -0200

    add amount attribute to dolalr class

--- src/com/rroggia/tddbyexample/financial/Dollar.java
+++ src/com/rroggia/tddbyexample/financial/Dollar.java
@@ -1,11 +1,12 @@
 package com.rroggia.tddbyexample.financial;
 
 public class Dollar {
-	
+	public int amount;
+
 	public Dollar(int amount) {
 	}
-	
-	public void times(int multiplier){
+
+	public void times(int multiplier) {
 	}
 
 }
