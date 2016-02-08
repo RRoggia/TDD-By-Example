45d77effd26845be86f2db542fe29c60b422d62e
RRoggia <renanzr@gmail.com>
2016-02-08 19:58:08 -0200

    import junit library

--- src/com/rroggia/tddbyexample/financial/MoneyTest.java
+++ src/com/rroggia/tddbyexample/financial/MoneyTest.java
@@ -1,9 +1,9 @@
 package com.rroggia.tddbyexample.financial;
+import static org.junit.Assert.assertEquals;
 
 public class MoneyTest {
 
-	
-	public void testMultiplication(){
+	public void testMultiplication() {
 		Dollar five = new Dollar(5);
 		five.times(2);
 		assertEquals(10, five.amount);
