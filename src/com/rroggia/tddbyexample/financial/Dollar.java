8dc5283f305402be199de132e8e6edabf048a3b6
Roggia <renan.roggia@sap.com>
2016-02-08 20:07:26 -0200

    change amount attribution to multiplier

--- src/com/rroggia/tddbyexample/financial/Dollar.java
+++ src/com/rroggia/tddbyexample/financial/Dollar.java
@@ -1,12 +1,13 @@
 package com.rroggia.tddbyexample.financial;
 
 public class Dollar {
-	public int amount = 5 * 2;
+	public int amount;
 
 	public Dollar(int amount) {
 	}
 
 	public void times(int multiplier) {
+		this.amount = 5 * 2;
 	}
 
 }
