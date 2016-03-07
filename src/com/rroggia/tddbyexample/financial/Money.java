014069045ddb20fa1705756b315c469d18d42fd7
RRoggia <renanzr@gmail.com>
2016-03-07 14:01:42 -0300

    change equals comparsion to currency instead of class

--- src/com/rroggia/tddbyexample/financial/Money.java
+++ src/com/rroggia/tddbyexample/financial/Money.java
@@ -12,7 +12,7 @@ public class Money {
 	@Override
 	public boolean equals(Object obj) {
 		Money money = (Money) obj;
-		return this.amount == money.amount && this.getClass().equals(money.getClass());
+		return this.amount == money.amount && this.currency.equals(money.currency());
 	}
 
 	public static Money dollar(int amount) {
@@ -23,15 +23,15 @@ public class Money {
 		return currency;
 	}
 
-	public Money times(int multiplier){
+	public Money times(int multiplier) {
 		return new Money(amount * multiplier, currency);
 	}
 
 	public static Money franc(int amount) {
 		return new Franc(amount, "CHF");
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return amount + " " + currency;
 	}
 
