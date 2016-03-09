c71f3c1d38e515aff3f8a01bc830a881a4f97724
RRoggia <renanzr@gmail.com>
2016-03-09 17:37:28 -0300

    add bank class to test

--- src/com/rroggia/tddbyexample/financial/MoneyTest.java
+++ src/com/rroggia/tddbyexample/financial/MoneyTest.java
@@ -32,6 +32,8 @@ public class MoneyTest {
 		Money sum = Money.dollar(5).plus(Money.dollar(5));
 		assertEquals(Money.dollar(10), sum);
 		
+		
+		Bank bank = new Bank();
 		Money reduced = bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(10), reduced);
 		
