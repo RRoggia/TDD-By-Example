c76f7e81a9288cfbb1c7377231b02b9cd9ab6e54
RRoggia <renanzr@gmail.com>
2016-03-09 17:35:54 -0300

    return a reduced expression after bank applies exchange rates

--- src/com/rroggia/tddbyexample/financial/MoneyTest.java
+++ src/com/rroggia/tddbyexample/financial/MoneyTest.java
@@ -32,6 +32,7 @@ public class MoneyTest {
 		Money sum = Money.dollar(5).plus(Money.dollar(5));
 		assertEquals(Money.dollar(10), sum);
 		
+		Money reduced = bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(10), reduced);
 		
 	}
