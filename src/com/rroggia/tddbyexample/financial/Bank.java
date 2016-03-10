68218364721dd4ece10739b4984c0a9a8239daad
RRoggia <renanzr@gmail.com>
2016-03-10 17:26:49 -0300

    return result of expression in reduce expression

--- src/com/rroggia/tddbyexample/financial/Bank.java
+++ src/com/rroggia/tddbyexample/financial/Bank.java
@@ -3,7 +3,9 @@ package com.rroggia.tddbyexample.financial;
 public class Bank {
 
 	public Money reduce(Expression source, String to) {
-		return Money.dollar(10);
+		Sum sum = (Sum) source;
+		int amount = sum.augend.amount + sum.addend.amount;
+		return new Money(amount, to);
 	}
 
 }
