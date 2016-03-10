f34a3ba9a6c3400bfc01fca7018d7b1632abd570
RRoggia <renanzr@gmail.com>
2016-03-10 16:46:54 -0300

    add Sum Constructor and implement Expression Interface to make tests pass

--- src/com/rroggia/tddbyexample/financial/Sum.java
+++ src/com/rroggia/tddbyexample/financial/Sum.java
@@ -1,16 +1,13 @@
 package com.rroggia.tddbyexample.financial;
 
-public class Sum {
+public class Sum implements Expression{
 
 	public Money augend;
 	public Money addend;
 	
 	public Sum(Money augend, Money addend) {
-		super();
 		this.augend = augend;
 		this.addend = addend;
 	}
-	
-	
-	
+		
 }
