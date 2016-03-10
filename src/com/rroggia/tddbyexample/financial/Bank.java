5aae5ef98e0aeacf45240b403b218c4a8e31c769
RRoggia <renanzr@gmail.com>
2016-03-10 17:37:15 -0300

    make test reduce money green - code with smells

--- src/com/rroggia/tddbyexample/financial/Bank.java
+++ src/com/rroggia/tddbyexample/financial/Bank.java
@@ -3,6 +3,7 @@ package com.rroggia.tddbyexample.financial;
 public class Bank {
 
 	public Money reduce(Expression source, String to) {
+		if (source instanceof Money) return (Money) source;
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
