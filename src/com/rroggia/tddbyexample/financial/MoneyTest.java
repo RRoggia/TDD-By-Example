c5d098b85a507f7c9f5bbc3ff1b99f29d90b6c4c
RRoggia <renanzr@gmail.com>
2016-03-10 17:26:49 -0300

    return result of expression in reduce expression

--- src/com/rroggia/tddbyexample/financial/MoneyTest.java
+++ src/com/rroggia/tddbyexample/financial/MoneyTest.java
@@ -17,7 +17,6 @@ public class MoneyTest {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 
@@ -34,7 +33,6 @@ public class MoneyTest {
 		Bank bank = new Bank();
 		Money reduced = bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(10), reduced);
-
 	}
 
 	@Test
