1d35208158aa6091a257c921ced2e6f750f86c1e
RRoggia <renanzr@gmail.com>
2016-03-10 17:14:29 -0300

    apply identation

--- src/com/rroggia/tddbyexample/financial/Sum.java
+++ src/com/rroggia/tddbyexample/financial/Sum.java
@@ -1,13 +1,13 @@
 package com.rroggia.tddbyexample.financial;
 
-public class Sum implements Expression{
+public class Sum implements Expression {
 
 	public Money augend;
 	public Money addend;
-	
+
 	public Sum(Money augend, Money addend) {
 		this.augend = augend;
 		this.addend = addend;
 	}
-		
+
 }
