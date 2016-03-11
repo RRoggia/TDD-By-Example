b564baf151f080ebaeb5cb10f1b6dddacc251175
RRoggia <renanzr@gmail.com>
2016-03-11 16:16:21 -0300

    change rate logic from Money to Bank

--- src/com/rroggia/tddbyexample/financial/Bank.java
+++ src/com/rroggia/tddbyexample/financial/Bank.java
@@ -4,6 +4,10 @@ import java.util.Currency;
 
 public class Bank {
 
+	public int rate(String from, String to) {
+		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+	}
+
 	public Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
