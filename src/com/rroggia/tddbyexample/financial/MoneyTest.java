8f3e4df07fef62c700cc8df74cb75882cd97f33e
RRoggia <renanzr@gmail.com>
2016-03-11 16:33:17 -0300

    add exchange rates table to bank

--- src/com/rroggia/tddbyexample/financial/MoneyTest.java
+++ src/com/rroggia/tddbyexample/financial/MoneyTest.java
@@ -60,13 +60,14 @@ public class MoneyTest {
 		Money result = bank.reduce(Money.dollar(1), "USD");
 		assertEquals(Money.dollar(1), result);
 	}
-	
+
 	@Test
-	public void testReduceMoneyDifferentCurrency(){
+	public void testReduceMoneyDifferentCurrency() {
 		Bank bank = new Bank();
-		bank.addRate("CHF","USD",2);
+		bank.addRate("CHF", "USD", 2);
 		Money result = bank.reduce(Money.franc(2), "USD");
-		assertEquals(Money.dollar(1), result);		
+		assertEquals(Money.dollar(1), result);
 	}
+
 
 }
