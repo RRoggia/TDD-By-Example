e662945e1e111d593d722a8ac527416321672700
RRoggia <renanzr@gmail.com>
2016-03-11 16:09:08 -0300

    add bank to interface of method reduce in Expression

--- src/com/rroggia/tddbyexample/financial/Bank.java
+++ src/com/rroggia/tddbyexample/financial/Bank.java
@@ -1,9 +1,11 @@
 package com.rroggia.tddbyexample.financial;
 
+import java.util.Currency;
+
 public class Bank {
 
 	public Money reduce(Expression source, String to) {
-		return source.reduce(to);
+		return source.reduce(this, to);
 	}
 
 }
