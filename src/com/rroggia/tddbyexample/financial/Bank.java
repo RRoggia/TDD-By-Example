5b5c46b6f445b6d24ab0857db098aa7288888b03
RRoggia <renanzr@gmail.com>
2016-03-11 16:33:17 -0300

    add exchange rates table to bank

--- src/com/rroggia/tddbyexample/financial/Bank.java
+++ src/com/rroggia/tddbyexample/financial/Bank.java
@@ -1,8 +1,10 @@
 package com.rroggia.tddbyexample.financial;
 
-import java.util.Currency;
+import java.util.Hashtable;
 
 public class Bank {
+
+	private Hashtable<Pair, Integer> rates = new Hashtable();
 
 	public int rate(String from, String to) {
 		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
@@ -10,6 +12,10 @@ public class Bank {
 
 	public Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
+	}
+
+	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), rate);
 	}
 
 }
